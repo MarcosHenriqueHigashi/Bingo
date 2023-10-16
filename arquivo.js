13e1caf76d5703127c2c4e90d945bdbab6e2fe00
Marcos Henrique Higashi <101603837+MarcosHenriqueHigashi@users.noreply.github.com>
2023-10-16 13:13:29 -0300

    Frist Commit

---
+++ arquivo.js
@@ -0,0 +1,451 @@
+let contador = 0
+function btn01(){ 
+    alert('Bola sorteada B 01 \nBoa Sorte !!!')
+    var a01="01"
+    document.getElementById('01').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a01}</strong>\u{1F603}`     
+}
+function btn02(){ 
+    alert('Bola sorteada B 02 \nBoa Sorte !!!')
+    var a02="02"
+    document.getElementById('02').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a02}</strong>\u{1F603}`     
+}
+function btn03(){ 
+    alert('Bola sorteada B 03 \nBoa Sorte !!!')
+    var a03="03"
+    document.getElementById('03').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a03}</strong>\u{1F603}`     
+}
+function btn04(){ 
+    alert('Bola sorteada B 04 \nBoa Sorte !!!')
+    var a04="04"
+    document.getElementById('04').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a04}</strong>\u{1F603}`     
+}
+function btn05(){ 
+    alert('Bola sorteada B 05 \nBoa Sorte !!!')
+    var a05="05"
+    document.getElementById('05').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a05}</strong>\u{1F603}`     
+}
+function btn06(){ 
+    alert('Bola sorteada B 06 \nBoa Sorte !!!')
+    var a06="06"
+    document.getElementById('06').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a06}</strong>\u{1F603}`     
+}
+function btn07(){ 
+    alert('Bola sorteada B 07 \nBoa Sorte !!!')
+    var a07="07"
+    document.getElementById('07').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a07}</strong>\u{1F603}`     
+}
+function btn08(){ 
+    alert('Bola sorteada B 08 \nBoa Sorte !!!')
+    var a08="08"
+    document.getElementById('08').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a08}</strong>\u{1F603}`     
+}
+function btn09(){ 
+    alert('Bola sorteada B 09 \nBoa Sorte !!!')
+    var a09="09"
+    document.getElementById('09').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a09}</strong>\u{1F603}`     
+}
+function btn10(){ 
+    alert('Bola sorteada B 10 \nBoa Sorte !!!')
+    var a10="10"
+    document.getElementById('10').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a10}</strong>\u{1F603}`     
+}
+function btn11(){ 
+    alert('Bola sorteada B 11 \nBoa Sorte !!!')
+    var a11="11"
+    document.getElementById('11').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a11}</strong>\u{1F603}`     
+}
+function btn12(){ 
+    alert('Bola sorteada B 12 \nBoa Sorte !!!')
+    var a12="12"
+    document.getElementById('12').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a12}</strong>\u{1F603}`     
+}
+function btn13(){ 
+    alert('Bola sorteada B 13 \nBoa Sorte !!!')
+    var a13="13"
+    document.getElementById('13').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a13}</strong>\u{1F603}`     
+}
+function btn14(){ 
+    alert('Bola sorteada B 14 \nBoa Sorte !!!')
+    var a14="14"
+    document.getElementById('14').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a14}</strong>\u{1F603}`     
+}
+function btn15(){ 
+    alert('Bola sorteada B 15 \nBoa Sorte !!!')
+    var a15="15"
+    document.getElementById('15').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a15}</strong>\u{1F603}`     
+}
+function btn16(){ 
+    alert('Bola sorteada I 16 \nBoa Sorte !!!')
+    var a16="16"
+    document.getElementById('16').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a16}</strong>\u{1F603}`     
+}
+function btn17(){ 
+    alert('Bola sorteada I 17 \nBoa Sorte !!!')
+    var a17="17"
+    document.getElementById('17').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a17}</strong>\u{1F603}`     
+}
+function btn18(){ 
+    alert('Bola sorteada I 18 \nBoa Sorte !!!')
+    var a18="18"
+    document.getElementById('18').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a18}</strong>\u{1F603}`     
+}
+function btn19(){ 
+    alert('Bola sorteada I 19 \nBoa Sorte !!!')
+    var a19="19"
+    document.getElementById('19').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a19}</strong>\u{1F603}`     
+}
+function btn20(){ 
+    alert('Bola sorteada I 20 \nBoa Sorte !!!')
+    var a02="20"
+    document.getElementById('20').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a20}</strong>\u{1F603}`     
+}
+function btn21(){ 
+    alert('Bola sorteada I 21 \nBoa Sorte !!!')
+    var a21="21"
+    document.getElementById('21').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a21}</strong>\u{1F603}`     
+}
+function btn22(){ 
+    alert('Bola sorteada I 22 \nBoa Sorte !!!')
+    var a22="22"
+    document.getElementById('22').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a22}</strong>\u{1F603}`     
+}
+function btn23(){ 
+    alert('Bola sorteada I 23 \nBoa Sorte !!!')
+    var a23="23"
+    document.getElementById('23').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a23}</strong>\u{1F603}`     
+}
+function btn24(){ 
+    alert('Bola sorteada I 24 \nBoa Sorte !!!')
+    var a24="24"
+    document.getElementById('24').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a24}</strong>\u{1F603}`     
+}
+function btn25(){ 
+    alert('Bola sorteada I 25 \nBoa Sorte !!!')
+    var a25="25"
+    document.getElementById('25').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a25}</strong>\u{1F603}`     
+}
+function btn26(){ 
+    alert('Bola sorteada I 26 \nBoa Sorte !!!')
+    var a26="26"
+    document.getElementById('26').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a26}</strong>\u{1F603}`     
+}
+function btn27(){ 
+    alert('Bola sorteada I 27 \nBoa Sorte !!!')
+    var a27="27"
+    document.getElementById('27').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a27}</strong>\u{1F603}`     
+}
+function btn28(){ 
+    alert('Bola sorteada I 28 \nBoa Sorte !!!')
+    var a28="28"
+    document.getElementById('28').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a28}</strong>\u{1F603}`     
+}
+function btn29(){ 
+    alert('Bola sorteada I 29 \nBoa Sorte !!!')
+    var a01="29"
+    document.getElementById('29').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a29}</strong>\u{1F603}`     
+}
+function btn30(){ 
+    alert('Bola sorteada I 30 \nBoa Sorte !!!')
+    var a30="30"
+    document.getElementById('30').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a30}</strong>\u{1F603}`     
+}
+function btn31(){ 
+    alert('Bola sorteada N 31 \nBoa Sorte !!!')
+    var a31="31"
+    document.getElementById('31').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a31}</strong>\u{1F603}`     
+}
+function btn32(){ 
+    alert('Bola sorteada N 32 \nBoa Sorte !!!')
+    var a32="32"
+    document.getElementById('32').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a32}</strong>\u{1F603}`     
+}
+function btn33(){ 
+    alert('Bola sorteada N 33 \nBoa Sorte !!!')
+    var a33="33"
+    document.getElementById('33').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a33}</strong>\u{1F603}`     
+}
+function btn34(){ 
+    alert('Bola sorteada N 34 \nBoa Sorte !!!')
+    var a34="34"
+    document.getElementById('34').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a34}</strong>\u{1F603}`     
+}
+function btn35(){ 
+    alert('Bola sorteada N 35 \nBoa Sorte !!!')
+    var a35="35"
+    document.getElementById('35').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a35}</strong>\u{1F603}`     
+}
+function btn36(){ 
+    alert('Bola sorteada N 36 \nBoa Sorte !!!')
+    var a36="36"
+    document.getElementById('36').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a36}</strong>\u{1F603}`     
+}
+function btn37(){ 
+    alert('Bola sorteada N 37 \nBoa Sorte !!!')
+    var a37="37"
+    document.getElementById('37').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a37}</strong>\u{1F603}`     
+}
+function btn38(){ 
+    alert('Bola sorteada N 38 \nBoa Sorte !!!')
+    var a38="38"
+    document.getElementById('38').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a38}</strong>\u{1F603}`     
+}
+function btn39(){ 
+    alert('Bola sorteada N 39 \nBoa Sorte !!!')
+    var a39="39"
+    document.getElementById('39').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a39}</strong>\u{1F603}`     
+}
+function btn40(){ 
+    alert('Bola sorteada N 40 \nBoa Sorte !!!')
+    var a40="40"
+    document.getElementById('40').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a40}</strong>\u{1F603}`     
+}
+function btn41(){ 
+    alert('Bola sorteada N 41 \nBoa Sorte !!!')
+    var a41="41"
+    document.getElementById('41').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a41}</strong>\u{1F603}`     
+}
+function btn42(){ 
+    alert('Bola sorteada N 42 \nBoa Sorte !!!')
+    var a42="42"
+    document.getElementById('42').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a42}</strong>\u{1F603}`     
+}
+function btn43(){ 
+    alert('Bola sorteada N 43 \nBoa Sorte !!!')
+    var a43="43"
+    document.getElementById('43').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a43}</strong>\u{1F603}`     
+}
+function btn44(){ 
+    alert('Bola sorteada N 44 \nBoa Sorte !!!')
+    var a44="44"
+    document.getElementById('44').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a44}</strong>\u{1F603}`     
+}
+function btn45(){ 
+    alert('Bola sorteada N 45 \nBoa Sorte !!!')
+    var a45="45"
+    document.getElementById('45').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a45}</strong>\u{1F603}`     
+}
+function btn46(){ 
+    alert('Bola sorteada G 46 \nBoa Sorte !!!')
+    var a46="46"
+    document.getElementById('46').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a46}</strong>\u{1F603}`     
+}
+function btn47(){ 
+    alert('Bola sorteada G 47 \nBoa Sorte !!!')
+    var a47="47"
+    document.getElementById('47').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a47}</strong>\u{1F603}`     
+}
+function btn48(){ 
+    alert('Bola sorteada G 48 \nBoa Sorte !!!')
+    var a48="48"
+    document.getElementById('48').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a48}</strong>\u{1F603}`     
+}
+function btn49(){ 
+    alert('Bola sorteada G 49 \nBoa Sorte !!!')
+    var a49="49"
+    document.getElementById('49').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a49}</strong>\u{1F603}`     
+}
+function btn50(){ 
+    alert('Bola sorteada G 50 \nBoa Sorte !!!')
+    var a50="50"
+    document.getElementById('50').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a05}</strong>\u{1F603}`     
+}
+function btn51(){ 
+    alert('Bola sorteada G 51 \nBoa Sorte !!!')
+    var a51="51"
+    document.getElementById('51').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a51}</strong>\u{1F603}`     
+}
+function btn52(){ 
+    alert('Bola sorteada G 52 \nBoa Sorte !!!')
+    var a52="52"
+    document.getElementById('52').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a52}</strong>\u{1F603}`     
+}
+function btn53(){ 
+    alert('Bola sorteada G 53 \nBoa Sorte !!!')
+    var a53="53"
+    document.getElementById('53').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a53}</strong>\u{1F603}`     
+}
+function btn54(){ 
+    alert('Bola sorteada G 54 \nBoa Sorte !!!')
+    var a54="54"
+    document.getElementById('54').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a54}</strong>\u{1F603}`     
+}
+function btn55(){ 
+    alert('Bola sorteada G 55 \nBoa Sorte !!!')
+    var a55="55"
+    document.getElementById('55').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a55}</strong>\u{1F603}`     
+}
+function btn56(){ 
+    alert('Bola sorteada G 56 \nBoa Sorte !!!')
+    var a56="56"
+    document.getElementById('56').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a56}</strong>\u{1F603}`     
+}
+function btn57(){ 
+    alert('Bola sorteada G 57 \nBoa Sorte !!!')
+    var a57="57"
+    document.getElementById('57').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a57}</strong>\u{1F603}`     
+}
+function btn58(){ 
+    alert('Bola sorteada G 58 \nBoa Sorte !!!')
+    var a58="58"
+    document.getElementById('58').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a58}</strong>\u{1F603}`     
+}
+function btn59(){ 
+    alert('Bola sorteada G 59 \nBoa Sorte !!!')
+    var a59="59"
+    document.getElementById('59').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a59}</strong>\u{1F603}`     
+}
+function btn60(){ 
+    alert('Bola sorteada G 60 \nBoa Sorte !!!')
+    var a60="60"
+    document.getElementById('60').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a60}</strong>\u{1F603}`     
+}
+function btn61(){ 
+    alert('Bola sorteada O 61 \nBoa Sorte !!!')
+    var a61="61"
+    document.getElementById('61').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a61}</strong>\u{1F603}`     
+}
+function btn62(){ 
+    alert('Bola sorteada O 62 \nBoa Sorte !!!')
+    var a62="62"
+    document.getElementById('62').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a62}</strong>\u{1F603}`     
+}
+function btn63(){ 
+    alert('Bola sorteada O 63 \nBoa Sorte !!!')
+    var a63="63"
+    document.getElementById('63').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a63}</strong>\u{1F603}`     
+}
+function btn64(){ 
+    alert('Bola sorteada O 64 \nBoa Sorte !!!')
+    var a64="64"
+    document.getElementById('64').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a64}</strong>\u{1F603}`     
+}
+function btn65(){ 
+    alert('Bola sorteada O 65 \nBoa Sorte !!!')
+    var a65="65"
+    document.getElementById('65').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a65}</strong>\u{1F603}`     
+}
+function btn66(){ 
+    alert('Bola sorteada O 66 \nBoa Sorte !!!')
+    var a66="66"
+    document.getElementById('66').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a66}</strong>\u{1F603}`     
+}
+function btn67(){ 
+    alert('Bola sorteada O 67 \nBoa Sorte !!!')
+    var a67="67"
+    document.getElementById('67').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a67}</strong>\u{1F603}`     
+}
+function btn68(){ 
+    alert('Bola sorteada O 68 \nBoa Sorte !!!')
+    var a68="68"
+    document.getElementById('68').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a68}</strong>\u{1F603}`     
+}
+function btn69(){ 
+    alert('Bola sorteada O 69 \nBoa Sorte !!!')
+    var a69="69"
+    document.getElementById('69').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a69}</strong>\u{1F603}`     
+}
+function btn70(){ 
+    alert('Bola sorteada O 70 \nBoa Sorte !!!')
+    var a70="70"
+    document.getElementById('70').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a70}</strong>\u{1F603}`     
+}
+function btn71(){ 
+    alert('Bola sorteada O 71 \nBoa Sorte !!!')
+    var a71="71"
+    document.getElementById('71').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a71}</strong>\u{1F603}`     
+}
+function btn72(){ 
+    alert('Bola sorteada O 72 \nBoa Sorte !!!')
+    var a72="72"
+    document.getElementById('72').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a72}</strong>\u{1F603}`     
+}
+function btn73(){ 
+    alert('Bola sorteada O 73 \nBoa Sorte !!!')
+    var a73="73"
+    document.getElementById('73').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a73}</strong>\u{1F603}`     
+}
+function btn74(){ 
+    alert('Bola sorteada O 74 \nBoa Sorte !!!')
+    var a74="74"
+    document.getElementById('74').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a74}</strong>\u{1F603}`     
+}
+function btn75(){ 
+    alert('Bola sorteada B 75 \nBoa Sorte !!!')
+    var a75="75"
+    document.getElementById('75').style.background='red'
+    res.innerHTML += `${++contador} N°<strong>${a75}</strong>\u{1F603}`     
+}
